aba35e95213207a028e5660fa9a00d22c9272f5c
frankuxui <fisdray@gmail.com>
2022-08-14 15:30:45 +0200

    card-21

--- src/App.jsx
+++ src/App.jsx
@@ -536,7 +536,7 @@ function App () {
             <div className='flex items-center space-x-4'>
               <img className='flex-none w-14 h-14 rounded-full object-cover' src='https://randomuser.me/api/portraits/women/55.jpg' alt='avatar' />
               <div className='flex-auto'>
-                <a href='#' className='text-lg font-bold block dark:text-white'>Monica Armas</a>
+                <a href='#' className='text-lg font-bold block dark:text-white'>Angelina Mconoor</a>
                 <span className='text-slate-500 dark:text-slate-400'>Dog Trainer</span>
               </div>
             </div>
@@ -665,6 +665,42 @@ function App () {
                 <span>169</span>
               </a>
             </div>
+          </div>
+        </div>
+
+        {/** card */}
+        <div className='break-inside p-6 flex flex-col justify-between rounded-xl mb-4 bg-white dark:bg-slate-800 dark:text-slate-50'>
+          <div className='flex justify-between'>
+            <div className='flex items-center space-x-4'>
+              <svg viewBox='0 0 24 24' width='28' height='28'>
+                <path fill='currentColor' d='M4.59,6.89C5.29,6.18 6,5.54 6.3,5.67C6.8,5.87 6.3,6.7 6,7.19C5.75,7.61 3.14,11.08 3.14,13.5C3.14,14.78 3.62,15.84 4.5,16.5C5.23,17.04 6.22,17.21 7.12,16.94C8.19,16.63 9.07,15.54 10.18,14.17C11.39,12.68 13,10.73 14.26,10.73C15.89,10.73 15.91,11.74 16,12.5C12.24,13.16 10.64,16.19 10.64,17.89C10.64,19.59 12.08,21 13.85,21C15.5,21 18.14,19.65 18.54,14.88H21V12.38H18.53C18.38,10.73 17.44,8.18 14.5,8.18C12.25,8.18 10.32,10.09 9.56,11C9,11.75 7.5,13.5 7.27,13.74C7,14.04 6.59,14.58 6.16,14.58C5.71,14.58 5.44,13.75 5.8,12.66C6.15,11.57 7.2,9.8 7.65,9.14C8.43,8 8.95,7.22 8.95,5.86C8.95,3.69 7.31,3 6.44,3C5.12,3 3.97,4 3.72,4.25C3.36,4.61 3.06,4.91 2.84,5.18L4.59,6.89M13.88,18.55C13.57,18.55 13.14,18.29 13.14,17.83C13.14,17.23 13.87,15.63 16,15.07C15.71,17.76 14.58,18.55 13.88,18.55Z' />
+              </svg>
+              <div className='flex-auto'>
+                <a href='#' className='text-xl font-bold block dark:text-white'>My draft</a>
+              </div>
+            </div>
+            <div className='flex items-center space-x-1 text-slate-500 dark:text-slate-400'>
+              <svg viewBox='0 0 24 24' width='18' height='18'>
+                <path fill='currentColor' d='M12 20C16.4 20 20 16.4 20 12S16.4 4 12 4 4 7.6 4 12 7.6 20 12 20M12 2C17.5 2 22 6.5 22 12S17.5 22 12 22C6.5 22 2 17.5 2 12C2 6.5 6.5 2 12 2M17 13.9L16.3 15.2L11 12.3V7H12.5V11.4L17 13.9Z' />
+              </svg>
+              <span className='uppercase text-sm font-medium'>3 min ago</span>
+            </div>
+          </div>
+          <div className='py-6 space-y-3'>
+            <h4 className='text-xl font-extrabold leading-snug'>Web Design templates Selection</h4>
+            <img src='https://images.pexels.com/photos/1158783/pexels-photo-1158783.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' className='rounded-xl h-[12rem] w-full object-cover' />
+            <p className='uppercase text-sm text-slate-500 font-medium dark:text-slate-400'>Introduction</p>
+            <p>Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.</p>
+          </div>
+          <div className='flex justify-between'>
+            <div className='flex items-center space-x-4'>
+              <a href='#' className='font-medium uppercase block dark:text-white'>Read more</a>
+            </div>
+            <button className='flex items-center justify-center rounded-full p-3 transition-all hover:bg-slate-200 dark:hover:bg-slate-700'>
+              <svg viewBox='0 0 24 24' width='22' height='22'>
+                <path fill='currentColor' d='M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z' />
+              </svg>
+            </button>
           </div>
         </div>
 
