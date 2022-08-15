291ac7ee47c473a51879ddee188baf76bbb11a77
frankuxui <fisdray@gmail.com>
2022-08-15 17:44:10 +0200

    card-30

--- src/App.jsx
+++ src/App.jsx
@@ -1022,12 +1022,69 @@ function App () {
               </button>
             </div>
             <div className='px-4 py-0 space-y-4'>
-              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmodelit sed do eiusmodelit sed do eiusmodelit sed do eiusmod <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#ui</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#uxui</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#userinterface</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#webdeveloper</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#card</a> </p>
+              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmodelit sed do eiusmodelit😨🥴🦊 sed do eiusmodelit sed do eiusmod <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#ui</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#uxui</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#userinterface</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#webdeveloper</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#card</a> </p>
             </div>
             <div className='p-4 bg-pink-600 text-white text-3xl font-medium leading-snug'>
               This impressive paella is a perfect party dish
             </div>
             <div className='p-4 flex flex-row justify-between'>
+              <a href='#' className='flex items-center font-medium space-x-1 group'>
+                <span className='p-3 rounded-full group-hover:bg-slate-100 dark:group-hover:bg-slate-700'>
+                  <svg viewBox='0 0 24 24' width='20' height='20' aria-hidden='true'>
+                    <g>
+                      <path fill='currentColor' d='M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z' />
+                    </g>
+                  </svg>
+                </span>
+                <span>43</span>
+              </a>
+              <a href='#' className='flex items-center font-medium space-x-1 group'>
+                <span className='p-3 rounded-full group-hover:bg-slate-100 dark:group-hover:bg-slate-700'>
+                  <svg viewBox='0 0 24 24' width='20' height='20' aria-hidden='true'>
+                    <g>
+                      <path fill='currentColor' d='M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z' />
+                      <path fill='currentColor' d='M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z' />
+                    </g>
+                  </svg>
+                </span>
+                <span>82</span>
+              </a>
+              <a href='#' className='flex items-center font-medium space-x-1 group'>
+                <span className='p-3 rounded-full group-hover:bg-slate-100 dark:group-hover:bg-slate-700'>
+                  <svg viewBox='0 0 24 24' width='20' height='20' aria-hidden='true'>
+                    <g>
+                      <path fill='currentColor' d='M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z' />
+                    </g>
+                  </svg>
+                </span>
+                <span>169</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/** card */}
+        <div className='break-inside flex flex-col justify-between rounded-xl mb-4 bg-white dark:bg-slate-800 dark:text-slate-50'>
+          <div className='space-y-4'>
+            <div className='pt-6 pr-6 pl-6 pb-4 flex justify-between flex-row items-center space-x-4 border-b dark:border-b-slate-700'>
+              <h1 className='text-2xl font-bold'>Web trends</h1>
+              <button className='flex items-center justify-center rounded-full p-2 transition-all hover:bg-slate-200 dark:hover:bg-slate-700'>
+                <svg width='26' height='26' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                  <path d='M20.71,7.04C20.37,7.38 20.04,7.71 20.03,8.04C20,8.36 20.34,8.69 20.66,9C21.14,9.5 21.61,9.95 21.59,10.44C21.57,10.93 21.06,11.44 20.55,11.94L16.42,16.08L15,14.66L19.25,10.42L18.29,9.46L16.87,10.87L13.12,7.12L16.96,3.29C17.35,2.9 18,2.9 18.37,3.29L20.71,5.63C21.1,6 21.1,6.65 20.71,7.04M3,17.25L12.56,7.68L16.31,11.43L6.75,21H3V17.25Z' fill='currentColor' />
+                </svg>
+              </button>
+            </div>
+            <div className='pt-4 pr-6 pl-6 pb-0 space-y-2'>
+              <h1 className='text-xl font-medium'>News</h1>
+              <p>14 May 2014</p>
+              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmodelit sed dosed do eiusmodelit sed do eiusmod <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#ui</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#uxui</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#userinterface</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#webdeveloper</a> <a href='#' className='font-medium text-blue-600 dark:text-slate-100'>#card</a> </p>
+            </div>
+            <div className='pr-6 pl-6'>
+              <div className='rounded-xl overflow-hidden'>
+                <img src='https://images.pexels.com/photos/2888489/pexels-photo-2888489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='Imagen' />
+              </div>
+            </div>
+            <div className='p-4 flex flex-row justify-evenly'>
               <a href='#' className='flex items-center font-medium space-x-1 group'>
                 <span className='p-3 rounded-full group-hover:bg-slate-100 dark:group-hover:bg-slate-700'>
                   <svg viewBox='0 0 24 24' width='20' height='20' aria-hidden='true'>
