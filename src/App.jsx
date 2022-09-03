85d370005a709b7918b47218b000340db130c54d
frankuxui <fisdray@gmail.com>
2022-09-03 22:42:59 +0200

    fixed-card

--- src/App.jsx
+++ src/App.jsx
@@ -606,8 +606,8 @@ function App () {
             <p className='text-[#BDBDBD]'>Minim dolor in amet nulla laboris enim dolore.</p>
           </div>
           <div className='p-4 flex flex-row justify-between'>
-            <a href='#' className='flex items-center font-medium space-x-1'>
-              <span className='p-3 rounded-full'>
+            <a href='#' className='flex items-center font-medium space-x-3'>
+              <span>
                 <svg viewBox='0 0 24 24' width='20' height='20' aria-hidden='true'>
                   <g>
                     <path fill='currentColor' d='M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z' />
@@ -616,8 +616,8 @@ function App () {
               </span>
               <span>43</span>
             </a>
-            <a href='#' className='flex items-center font-medium space-x-1'>
-              <span className='p-3 rounded-full'>
+            <a href='#' className='flex items-center font-medium space-x-3'>
+              <span>
                 <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'>
                   <circle cx='18' cy='5' r='3' />
                   <circle cx='6' cy='12' r='3' />
@@ -628,8 +628,8 @@ function App () {
               </span>
               <span>82</span>
             </a>
-            <a href='#' className='flex items-center font-medium space-x-1'>
-              <span className='p-3 rounded-full'>
+            <a href='#' className='flex items-center font-medium space-x-3'>
+              <span>
                 <svg viewBox='0 0 24 24' width='20' height='20' aria-hidden='true'>
                   <g>
                     <path fill='currentColor' d='M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z' />
