1a8cc05290525789217a283c364d19131217ce3a
frankuxui <fisdray@gmail.com>
2022-12-03 22:11:36 +0100

    open-graph

--- src/components/OpenGraph.jsx
+++ src/components/OpenGraph.jsx
@@ -7,22 +7,39 @@ const OpenGraph = ({ title, description, image, path }) => {
     <Helmet>
       <meta charSet='UTF-8' />
       <link rel='icon' type='image/svg+xml' href='/src/favicon.svg' />
-      <link rel='canonical' href={path} />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <title>{title || 'React cards component'}</title>
       <meta property='og:type' content='website' />
-      <meta property='og:title' content={title || 'React cards component'} />
-      <meta property='og:description' content={description} />
-      <meta property='og:site_name' content={description} />
-      <meta property='og:url' content={path} />
-      <meta property='og:image' content={image} />
+      <meta property='og:title' content='React card components' />
+      <meta
+        property='og:description'
+        content=''
+      />
+      <meta property='og:site_name' content='React cards components with vite js and tailwindcss' />
+      <meta
+        property='og:url'
+        content='https://react-tailwind-components-xi.vercel.app/'
+      />
+      <meta
+        property='og:image'
+        content='https://cards-components.vercel.app/assets/cover.ce42f005.jpg'
+      />
       <meta property='og:image:width' content='1660' />
       <meta property='og:image:height' content='876' />
       <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:url' content={path} />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={image} />
+      <meta
+        name='twitter:url'
+        content='https://react-tailwind-components-xi.vercel.app/'
+      />
+      <meta name='twitter:title' content='React cards component' />
+      <meta
+        name='twitter:description'
+        content='React cards components with vite js and tailwindcss'
+      />
+      <meta
+        name='twitter:image'
+        content='https://cards-components.vercel.app/assets/cover.ce42f005.jpg'
+      />
+      <title>React card components</title>
     </Helmet>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import OpenGraph from './components/OpenGraph'
-import content from '../content.json'
+// import content from '../content.json'
 
 ReactDOM.render(
   <React.StrictMode>
-    <OpenGraph
-      title={content.title}
-      description={content.description}
-      image={content.image}
-      path={content.path}
-    />
+    <OpenGraph />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
