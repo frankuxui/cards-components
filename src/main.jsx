58c673164495550cb55592570c4e9d94202996f6
frankuxui <fisdray@gmail.com>
2022-12-03 22:00:39 +0100

    open-graph

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import OpenGraph from './components/OpenGraph'
+import content from '../content.json'
 
 ReactDOM.render(
   <React.StrictMode>
+    <OpenGraph
+      title={content.title}
+      description={content.description}
+      image={content.image}
+      path={content.path}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
